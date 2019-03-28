d7be52bf9dc4651dfc522856f777fccdabc88d76
Robert Carlos <robert.carlos@linuxmail.org>
2019-03-27 21:44:52 -0300

    Removido diretório codes que possuia os mesmo arquivos do diretório python3

--- codes/1-Say_Hello_World!_With_Python.py
+++
@@ -1,4 +0,0 @@
-# 1-Say "Hello, World!" With Python.py
-
-if __name__ == '__main__':
-    print("Hello, World!")
--- codes/14-Triangle_Quest.py
+++
@@ -1,7 +0,0 @@
-# Triangle Quest
-
-
-if __name__ == '__main__':
-    for i in range(1,int(input())):
-        print (int((10 ** i - 1) / 9 * i))
-
