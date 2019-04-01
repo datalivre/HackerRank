557fb205e105b982ca7b5fc5457413f689170dbb
Robert Carlos <robert.carlos@linuxmail.org>
2019-04-01 02:16:09 -0300

    Diretórios renomeados.

--- python3/Built-Ins/input.py
+++ python3/Built-Ins/input.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python2.7
 # Input()
 # https://www.hackerrank.com/challenges/input/problem
-
 
 def compare(x, k):
     return input() == k
 
 
 if __name__ == "__main__":
+    # py2
     x, k = map(int, raw_input().split())
     print(compare(x, k))
