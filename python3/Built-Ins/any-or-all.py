c1340f92a8af1ec5183196d5fef692bafbe14731
Robert Carlos <robert.carlos@linuxmail.org>
2019-04-02 23:48:07 -0300

    Foi deletado um arquivo duplicado e outro corrigido.

--- python3/Built-Ins/any-or-all.py
+++ python3/Built-Ins/any-or-all.py
@@ -2,5 +2,5 @@
 # https://www.hackerrank.com/challenges/any-or-all/problem
 
 
-n, lista = input(), input().split()
+lista = [input().split() for _ in range(2)][1]
 print(all(int(i) >= 0 for i in lista) and any(i == i[::-1] for i in lista))
